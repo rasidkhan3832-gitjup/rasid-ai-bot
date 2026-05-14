b5a7ff4c75b42a9da34424ef7651cfb24dbb0925
rasidkhan3832-gitjup <rasidkhan3832@gmail.com>
2026-05-14 23:37:22 +0530

    Update main.py
    Add Telegram bot

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Rasid AI Bot Running")
+import requests
+import time
+
+BOT_TOKEN = "8737848238:AAGNE41e3qZuakLMu10DrgxEh69NqUoSNq4"
+CHAT_ID = "5745714729"
+
+def send_signal(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+
+    requests.post(url, data=data)
+
+while True:
+    signal = "📈 BUY SIGNAL EUR/USD"
+    send_signal(signal)
+    print("Signal Sent")
+
+    time.sleep(60)
